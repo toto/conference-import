93a6a925c9e4233d634ade27748be1c02bfa42de
Thomas Kollbach <thomas@kollba.ch>
2019-03-31 21:47:30 +0200

    remove files

--- src/models/basic.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var ItemType;
-(function (ItemType) {
-    ItemType["speaker"] = "speaker";
-    ItemType["session"] = "session";
-})(ItemType = exports.ItemType || (exports.ItemType = {}));
