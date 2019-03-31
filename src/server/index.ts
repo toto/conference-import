07e8ac105a02cf79a2e1c4f8513bd1d158dee5ea
Thomas Kollbach <thomas@kollba.ch>
2019-03-31 18:42:20 +0200

    add detail support

--- src/server/index.ts
+++ src/server/index.ts
@@ -27,13 +27,34 @@ app.get('/events', (req, res) => {
   const path = `/${data.event.id}/${resource}`;
   
   app.get(path, (req, res) => {
-    console.info(`Getting ${path}`);
+    console.info(`Getting ${req.path}`);
     if (anyData[resource]) {
       return res.json(wrapInResponseData(anyData[resource] as any[]));
     } else {
       return res.json(wrapInResponseData([]));
     }
-  });  
+  });
+
+  const detailPath = new RegExp(`/${data.event.id}/${resource}/([a-zA-Z0-9_\-]+)`);
+  app.get(detailPath, (req, res) => {
+    console.info(`Getting ${req.path}`);
+
+    const resourceId = req.params[0];
+    if (!resourceId) {
+      return res.status(404).json(wrapInResponseData([]));
+    }
+    const resourceData = anyData[resource] as {id: string}[];
+    if (!resourceData) {
+      return res.status(404).json(wrapInResponseData([]));
+    }
+
+    const singularResource = resourceData.find(r => r.id === resourceId);
+    if (!singularResource) {
+      return res.status(404).json(wrapInResponseData([]));
+    }
+
+    return res.json(wrapInResponseData([singularResource]));
+  });
 });
 
 app.listen(PORT, 'localhost', () => console.info(`API listening on port ${PORT}`));
