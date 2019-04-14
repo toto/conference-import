bc6f656fc9774a9f15e0b73f6f83cbaeea004504
Thomas Kollbach <thomas@kollba.ch>
2019-04-14 18:23:35 +0200

    Update serve/import calling

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,48 @@
 import * as process from 'process';
 import { Configuration, dumpNormalizedConference } from './importer/dumper';
-import { readFileSync } from 'fs';
+import { serveEvents } from './server';
+import { readFileSync, writeFileSync } from 'fs';
+import * as minimist from 'minimist';
+import * as path from "path";
 
-const configFile = process.argv[process.argv.length - 2]
-const exportDir = process.argv[process.argv.length - 1]
-const config = JSON.parse(readFileSync(configFile, 'utf8')) as Configuration;
+enum COMMAND {
+  IMPORT = 'import',
+  SERVE = 'serve'
+}
 
-dumpNormalizedConference(config, exportDir).then(() => { 
-  console.info('Export to', exportDir);
-}).catch(error => console.error(error));
+function printHelp() {
+  console.log("Help:");
+  console.log("  -h|--help\tprint this help");
+}
+
+const argv = minimist(process.argv, {'--': true, alias: {pid: 'p', help: 'h'}});
+if (argv['help'] || argv._.length === 0) {
+  printHelp();
+  process.exit(0);
+}
+
+if (argv[COMMAND.IMPORT]) {
+  const configFile = process.argv[argv._.length - 2]
+  const exportDir = process.argv[argv._.length - 1]
+  const config = JSON.parse(readFileSync(configFile, 'utf8')) as Configuration;
+  
+  dumpNormalizedConference(config, exportDir).then(() => { 
+    console.info('Exported to', exportDir);
+  }).catch(error => console.error(error));  
+
+} else if (argv[COMMAND.SERVE] && argv["--"]) {
+  if (argv.pid) {
+    writeFileSync(argv.pid, `${process.pid}`, {flag: 'w'});
+    console.info(`Writing PID file to ${argv.pid}`);
+  }
+
+  const paths = argv["--"].filter(arg => arg !== COMMAND.SERVE);
+  const files = paths.map(p => path.normalize(p));
+  serveEvents(files).then(() => {
+    console.log('Serving…');
+  }).catch(error => console.error(error));
+} else {
+  printHelp();
+}
+
+    
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,7 @@
-import * as path from "path";
 import * as express from "express";
 import { EventDataStore } from "./eventDataStore";
 
-const PORT = 5000;
-const app = express();
+
 
 interface Response<T> {
   ok: boolean;
@@ -15,84 +13,77 @@ function wrapInResponseData<T>(data: T[]): Response<T> {
   return { ok: true, count: data.length, data };
 }
 
-const files: string[] = [];
-
-if (process.argv.length <= 2) {
-  const [argv0, argv1] = process.argv;
-  console.error(`Usage: ${argv0} ${argv1} file0 file1.json...`);
-  process.exit(-1);
-}
-
-for (let i = 2; i < process.argv.length; i++) {
-  files.push(path.normalize(process.argv[i]));
-}
-
-const stores: Map<string, EventDataStore> = new Map();
-
-files.forEach(jsonPath => {
-  const store = EventDataStore.eventDataFromFile(jsonPath);
-  if (!store) return;
-  const values = Array.from(store.sessions.values());
-  console.info(`Serving ${store.event.label} (${store.event.id}, ${values.length} sessions)`);
-  stores.set(store.event.id, store);
-});
-
-app.get("/events", (req, res) => {
-  const events = Array.from(stores.values()).map(s => s.event);
-  return res.json(wrapInResponseData(events));
-});
-
-// Unimplemented
-["pois", "maps"].forEach(resource => {
-  const regexp = new RegExp(`/[a-z09A-Z]+\/${resource}`);
-  app.get(regexp, (req, res) => {
-    console.info(`Getting ${req.path} (unimplemnted)`);
-    return res.status(404).json(wrapInResponseData([]));
-  });
-});
-
-const resourceName = [
-  "sessions",
-  "speakers",
-  "days",
-  "tracks",
-  "locations",
-  "subconferences"
-];
-resourceName.forEach(resource => {
-  const detailPath = new RegExp(
-    `\/([a-z0-9A-Z_\-]+)\/${resource}\/([a-zA-Z0-9_\-]+)`
-  );
-  const allPath = new RegExp(
-    `\/([a-z0-9A-Z_\-]+)\/${resource}`
-  );
-  
-  app.get([detailPath, allPath], (req, res) => {
-    console.info(`Getting ${req.path}`);
-
-    const eventId = req.params[0];
-    if (!eventId) return res.status(404).json(wrapInResponseData([]));
-
-    const store = stores.get(eventId);
-    if (!store) return res.status(404).json(wrapInResponseData([]));
-
-    const resourceId = req.params[1];
-    if (resourceId) {
-      const singularResource = store.resourceForId(resource as any, resourceId);
-      if (!singularResource) {
-        return res.status(404).json(wrapInResponseData([]));
-      }
-      return res.json(wrapInResponseData([singularResource]));
-    } else {
-      const resources = store.resources(resource as any);
-      return res.json(wrapInResponseData(resources));
-    }
-
-
+export async function serveEvents(files: string[], server='localhost', port=5000) {
+  return new Promise((resolve) => {
+    const app = express();
     
-  });
-});
+    const stores: Map<string, EventDataStore> = new Map();
+    
+    files.forEach(jsonPath => {
+      const store = EventDataStore.eventDataFromFile(jsonPath);
+      if (!store) return;
+      const values = Array.from(store.sessions.values());
+      console.info(`Serving ${store.event.label} (${store.event.id}, ${values.length} sessions)`);
+      stores.set(store.event.id, store);
+    });
+    
+    app.get("/events", (req, res) => {
+      const events = Array.from(stores.values()).map(s => s.event);
+      return res.json(wrapInResponseData(events));
+    });
+    
+    // Unimplemented
+    ["pois", "maps"].forEach(resource => {
+      const regexp = new RegExp(`/[a-z09A-Z]+\/${resource}`);
+      app.get(regexp, (req, res) => {
+        console.info(`Getting ${req.path} (unimplemnted)`);
+        return res.status(404).json(wrapInResponseData([]));
+      });
+    });
+    
+    const resourceName = [
+      "sessions",
+      "speakers",
+      "days",
+      "tracks",
+      "locations",
+      "subconferences"
+    ];
+    resourceName.forEach(resource => {
+      const detailPath = new RegExp(
+        `\/([a-z0-9A-Z_\-]+)\/${resource}\/([a-zA-Z0-9_\-]+)`
+      );
+      const allPath = new RegExp(
+        `\/([a-z0-9A-Z_\-]+)\/${resource}`
+      );
+      
+      app.get([detailPath, allPath], (req, res) => {
+        console.info(`Getting ${req.path}`);
+    
+        const eventId = req.params[0];
+        if (!eventId) return res.status(404).json(wrapInResponseData([]));
+    
+        const store = stores.get(eventId);
+        if (!store) return res.status(404).json(wrapInResponseData([]));
+    
+        const resourceId = req.params[1];
+        if (resourceId) {
+          const singularResource = store.resourceForId(resource as any, resourceId);
+          if (!singularResource) {
+            return res.status(404).json(wrapInResponseData([]));
+          }
+          return res.json(wrapInResponseData([singularResource]));
+        } else {
+          const resources = store.resources(resource as any);
+          return res.json(wrapInResponseData(resources));
+        }
+      });
+    });
+    
+    app.listen(port, "localhost", () => {
+      console.info(`API listening on port ${port}`)
+      resolve();
+    });  
+  });  
+}
 
-app.listen(PORT, "localhost", () =>
-  console.info(`API listening on port ${PORT}`)
-);
