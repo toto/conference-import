304cf25e069d41dd8e326a3a2e59c6ac1a0c594a
Thomas Kollbach <thomas@kollba.ch>
2021-12-19 14:56:16 +0100

    fix linter errors

--- src/dataSources/frab/converters.ts
+++ src/dataSources/frab/converters.ts
@@ -1,18 +1,18 @@
 import * as moment from 'moment-timezone';
 import { FrabDataSourceFormat } from "./dataFormat";
-import { Session, Language, MiniSpeaker, MiniTrack, Speaker, MiniSession, MiniLocation } from "../../models";
+import { Session, MiniSpeaker, MiniTrack, Speaker, MiniSession, MiniLocation } from "../../models";
 import { mkId, dehtml } from "../rp/utils";
-import { languageFromIsoCode } from "../rp/language";
+import { languageFromIsoCode, English } from "../rp/language";
 import { normalizedForId } from '../util';
 
-function speakerFromJson(json: any, config: FrabDataSourceFormat): Speaker | undefined {
+function speakerFromJson(json: Record<string, string>, config: FrabDataSourceFormat): Speaker | undefined {
   if (!config.frabBaseUrl) return undefined;
   let biography = '';
   if (json.abstract) biography += json.abstract;
   if (json.abstract && json.description) biography += "\n\n";
   if (json.description) biography += json.description;
 
-  const sessions: MiniSession[] = json.events.map((miniSession: any) => {
+  const sessions: MiniSession[] = (json.events as unknown as Record<string, string>[]).map((miniSession: Record<string, string>) => {
     let id = config.subconferenceId && config.useSubconferenceIdInSessionId === true ? `${config.subconferenceId}-${miniSession.id}` : `${miniSession.id}`;
     if (config.preferGuid === true && miniSession.guid) {
       id = miniSession.guid;
@@ -48,29 +48,29 @@ function speakerFromJson(json: any, config: FrabDataSourceFormat): Speaker | und
   return result;
 }
 
-export function speakersFromJson(json: any, config: FrabDataSourceFormat): Speaker[] {
+export function speakersFromJson(json: Record<string, unknown>, config: FrabDataSourceFormat): Speaker[] {
   const { schedule_speakers } = json;
   if (!schedule_speakers) return [];
-  const { speakers } = schedule_speakers;
+  const { speakers } = schedule_speakers as {speakers: Record<string, string>[]};
   if (!speakers || !Array.isArray(speakers)) return [];
 
-  const speakerResult = speakers.map((s: any) => speakerFromJson(s, config));
+  const speakerResult = speakers.map(s => speakerFromJson(s, config));
   return speakerResult.filter(s => s !== undefined) as Speaker[];
 }
 
-export function sessionsFromJson(json: any, config: FrabDataSourceFormat): Session[] {
+export function sessionsFromJson(json: Record<string, unknown>, config: FrabDataSourceFormat): Session[] {
   const { schedule } = json;
   if (!schedule) return [];
-  const { conference } = schedule;
+  const { conference } = schedule as {conference: Record<string, unknown>};
   if (!conference) return [];
-  const { days } = conference;
-  if (!days) return [];
+  const { days } = conference as {days: Record<string, unknown>[]};
+  if (!days || !Array.isArray(days)) return [];
 
   const { ignoredLocationNames } = config;
 
   const result: Session[] = [];
   for (const day of days) {
-    const { rooms } = day;
+    const { rooms, date } = day as {rooms: Record<string, string>[], date: string};
     if (!rooms) continue;
 
     for (const roomName in rooms) {
@@ -82,7 +82,7 @@ export function sessionsFromJson(json: any, config: FrabDataSourceFormat): Sessi
       if (!Array.isArray(sessionsForRoom)) continue;
 
       for (const sessionJson of sessionsForRoom) {
-        const session = parseSession(day.date, roomName, sessionJson, config);
+        const session = parseSession(date, roomName, sessionJson, config);
         if (session) result.push(session);
       }
     }
@@ -90,13 +90,13 @@ export function sessionsFromJson(json: any, config: FrabDataSourceFormat): Sessi
   return result;
 }
 
-function parseSession(date: string, roomName: string, session: any, config: FrabDataSourceFormat): Session | undefined {
+function parseSession(date: string, roomName: string, session: Record<string, unknown>, config: FrabDataSourceFormat): Session | undefined {
   let track: MiniTrack = config.defaultTrack;
   if (session.track) {
     track = {
       id: mkId(`${config.eventId}-${session.track}`),
-      label_en: session.track,
-      label_de: session.track,
+      label_en: session.track as string,
+      label_de: session.track as string,
     };
   }
 
@@ -111,15 +111,15 @@ function parseSession(date: string, roomName: string, session: any, config: Frab
     label_en: roomName
   };
 
-  let language: Language = languageFromIsoCode(config.defaultLanguageCode)!;
+  let language = languageFromIsoCode(config.defaultLanguageCode);
   if (session.language) {
-    const sessionLang = languageFromIsoCode(session.language);
+    const sessionLang = languageFromIsoCode(session.language as string);
     if (sessionLang) language = sessionLang;
   }
 
-  let persons = session.persons;
+  let persons = session.persons as {id: number, public_name: string}[];
   if (!persons) persons = [];
-  let speakers: MiniSpeaker[] = persons.map((person: {id: number, public_name: string}) => {
+  let speakers: MiniSpeaker[] = persons.map(person => {
     let id = `${person.id}`;
     if (config.baseSpeakerIdOnName === true) {
       id = normalizedForId(person.public_name);
@@ -136,9 +136,9 @@ function parseSession(date: string, roomName: string, session: any, config: Frab
   let end: moment.Moment | undefined;
   
   if (session.date && session.duration) {
-    begin = moment(session.date);
-    const [ hoursStr, minutesStr ] = session.duration.split(':');
-    end = moment(session.date);
+    begin = moment(session.date as string);
+    const [ hoursStr, minutesStr ] = (session.duration as string).split(':');
+    end = moment(session.date as string);
     end.add(parseInt(hoursStr), 'h');
     end.add(parseInt(minutesStr), 'm');
   }
@@ -150,9 +150,9 @@ function parseSession(date: string, roomName: string, session: any, config: Frab
   
   let sessionId = `${session.id}`;
   if (config.preferGuid === true && session.guid) {
-    sessionId = session.guid;
+    sessionId = session.guid as string;
   } else if (session.guid && !session.id) {
-    sessionId = session.guid;
+    sessionId = session.guid as string;
   }
 
   if (sessionId.length === 0) {
@@ -164,25 +164,25 @@ function parseSession(date: string, roomName: string, session: any, config: Frab
     sessionId = `${config.subconferenceId}-${sessionId}`;
   }
 
-  let { url } = session;
+  const { url } = session as { url: string };
   // We need a url
-  if (!url || !url.match(/https?\:\/\//)) {
-    console.warn(`Invalid url for: ${session.id} (${session.title}) becuase of invalid URL '${url}'`);
-    url = null;
+  if (!url || !url.match(/https?:\/\//)) {
+    console.error(`Invalid url for: ${session.id} (${session.title}) becuase of invalid URL '${url}'. Not importing session`);
+    return undefined;
   }
 
   const result: Session = {
     type: "session",
     event: config.eventId,
     id: config.prefixSessionsWithEventId === true ? `${config.eventId}-${sessionId}` : sessionId,
-    title: session.title,
-    subtitle: session.subtitle,
-    abstract: session.abstract,
-    description: dehtml(session.description),
+    title: session.title as string,
+    subtitle: session.subtitle as string | undefined,
+    abstract: session.abstract as string,
+    description: dehtml(session.description as string),
     url,
     track,
     location,
-    lang: language,
+    lang: language ?? English,
     enclosures: [],
     speakers,
     related_sessions: [],
--- src/dataSources/rp/language.ts
+++ src/dataSources/rp/language.ts
@@ -1,25 +1,25 @@
 import { Language } from './../../models';
 
-const english: Language = {
+export const English: Language = {
   id: 'en',
   label_en: 'English',
   label_de: 'Englisch',
 }
 
-const german: Language = {
+export const German: Language = {
   id: 'de',
   label_en: 'German',
   label_de: 'Deutsch',
 }
 
 export function languageFromString(str: string): Language | null {
-  if (str === 'English') return english;
-  if (str === 'German') return german;
+  if (str === 'English') return English;
+  if (str === 'German') return German;
   return null;
 }
 
 export function languageFromIsoCode(str: string): Language | null {
-  if (str === 'en') return english;
-  if (str === 'de') return german;
+  if (str === 'en') return English;
+  if (str === 'de') return German;
   return null;
 }
