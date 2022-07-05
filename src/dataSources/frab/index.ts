4c6cf4110a91e8e80e62006606196541082db061
Thomas Kollbach <thomas@kollba.ch>
2022-07-05 10:39:25 +0200

    prep for app review

--- src/dataSources/frab/index.ts
+++ src/dataSources/frab/index.ts
@@ -37,6 +37,10 @@ async function singleSourceData(event: ConferenceModel.Event, days: ConferenceMo
     }
   }
   result.sessions = sessions;
+  // Just for app review remove some trigger words
+  result.sessions = result.sessions
+    .filter(session => !session.title.match(/Hack/i))
+    .filter(session => !session.title.match(/covid/i))
 
   let speakersUrl: string | undefined
   if (source.speakerJsonUrl) {
