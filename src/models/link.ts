79e142f5adbb97b988a729463f4714454537715b
Thomas Kollbach <thomas@kollba.ch>
2023-12-13 10:42:47 +0100

    add more allowed social networks

--- src/models/link.ts
+++ src/models/link.ts
@@ -1,8 +1,8 @@
 export interface Link {
   url: string
-  type: "speaker-link" | "recording" | "livestream" | "session-link"
+  type: "speaker-link" | "recording" | "livestream" | "session-link" | "feedback-link"
   title: string
-  service: "web" | "twitter" | "facebook" | "github" | "youtube" | "vimeo"
+  service: "web" | "twitter" | "facebook" | "github" | "youtube" | "vimeo" | "mastodon" | "bluesky" | "threads" | "vimeo"
   thumbnail?: string
   languages?: string[]
 }
