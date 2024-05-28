1c93baba5ee84c8fa2fa7ef2531aba58ebd60b42
Thomas Kollbach <thomas@kollba.ch>
2024-05-28 17:33:36 +0200

    infos

--- src/dataSources/rp2/youtube.ts
+++ src/dataSources/rp2/youtube.ts
@@ -14,6 +14,8 @@ export async function youtubeUrlByTitle(playlistId: string, prefix = "re:publica
   let stdout: string | undefined
 
   try {
+    const which = await execAsync(`which yt-dlp`)
+    console.info(which.stdout)
     const res = await execAsync(`yt-dlp --flat-playlist  -J "https://www.youtube.com/watch?list=${playlistId}"`)
     stdout = res.stdout;
     if (!stdout) {
