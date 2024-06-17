15cba46e7143e029be806b61176307d06571213b
Thomas Kollbach <thomas@kollba.ch>
2024-06-17 15:04:16 +0200

    add -J option

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { serveEvents } from './server';
 import { readFileSync, writeFileSync, unlinkSync } from 'fs';
 import * as minimist from 'minimist';
 import * as path from "path";
+import * as fs from "fs";
 
 enum COMMAND {
   IMPORT = 'import',
@@ -20,7 +21,8 @@ const argv = minimist(process.argv,
    alias: {
       pid: 'p', 
       help: 'h', 
-      out: 'o'
+      out: 'o',
+      jsonDir: 'J'
     }
   });
 if (argv['help'] || argv._.length === 0) {
@@ -38,6 +40,28 @@ if (argv[COMMAND.IMPORT] && argv.config && argv.out) {
   }).catch(error => console.error(error.toString()));  
 
 } else if (argv[COMMAND.SERVE] && argv["--"]) {
+
+  let files: string[] = [];
+
+  if (argv.jsonDir) {
+    const dirPath = path.normalize(argv.jsonDir)
+    console.info(dirPath)
+    const dirEntries = fs.readdirSync(dirPath, {withFileTypes: true})
+    for (const dirEnt of dirEntries) {
+      if (!dirEnt.isFile()) continue;
+      const filepath = path.join(dirPath, dirEnt.name);
+      files.push(filepath);
+    } 
+  } else if (argv["--"]) {
+    const paths = argv["--"].filter(arg => arg !== COMMAND.SERVE);
+    files = paths.map(p => path.normalize(p));
+  }
+
+  if (files.length == 0) {
+    console.error("No files to serve. Use -J or pass files after -- at the end")
+    process.exit(-1);
+  }
+
   if (argv.pid) {
     writeFileSync(argv.pid, `${process.pid}`, {flag: 'w'});
     process.on('SIGTERM', function () {
@@ -47,8 +71,7 @@ if (argv[COMMAND.IMPORT] && argv.config && argv.out) {
     console.info(`Writing PID file to ${argv.pid}`);
   }
 
-  const paths = argv["--"].filter(arg => arg !== COMMAND.SERVE);
-  const files = paths.map(p => path.normalize(p));
+  
   serveEvents(files).then(() => {
     console.log('Serving…');
   }).catch(error => console.error(error));
