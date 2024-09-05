a7a3888aafc055650c6e7e2d26a8820486a18c61
Thomas Kollbach <thomas@kollba.ch>
2024-09-05 15:28:43 +0200

    Expand link

--- src/models/link.ts
+++ src/models/link.ts
@@ -1,8 +1,8 @@
 export interface Link {
   url: string
-  type: "speaker-link" | "recording" | "livestream" | "session-link" | "feedback-link" | "session-alternate"
+  type: "speaker-link" | "recording" | "livestream" | "session-link" | "feedback-link" | "session-alternate" | "social-interaction"
   title: string
-  service: "web" | "twitter" | "facebook" | "github" | "youtube" | "vimeo" | "mastodon" | "bluesky" | "threads" | "vimeo" | "livevoice"
+  service: "web" | "twitter" | "facebook" | "github" | "youtube" | "vimeo" | "mastodon" | "bluesky" | "threads" | "vimeo" | "livevoice" | "activityPub"
   thumbnail?: string
   languages?: string[]
 }
